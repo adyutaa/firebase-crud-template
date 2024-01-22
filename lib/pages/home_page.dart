3380fe6edf67c09f446c135e07c59655e5d10220
Adyuta Indra Adyatma <adyuta24.hr@gmail.com>
2024-01-22 13:11:38 +0700

    successfully add a new note to the db

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,7 +1,41 @@
+import 'package:crud_template/database/firestore.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  //text controller
+  final TextEditingController textController = TextEditingController();
+
+  final FirestoreService firestoreService = FirestoreService();
+
+  // open dialog box to add a new note
+  void openNoteBox() {
+    showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+              content: TextField(
+                controller: textController,
+              ),
+              actions: [
+                ElevatedButton(
+                    onPressed: () {
+                      // add a new note button function
+                      firestoreService.addNote(textController.text);
+                      //clear the text
+                      textController.clear();
+                      // dismiss the dialog box
+                      Navigator.pop(context);
+                    },
+                    child: Text("add"))
+              ],
+            ));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,7 +44,7 @@ class HomePage extends StatelessWidget {
           title: Text("Notes"),
         ),
         floatingActionButton: FloatingActionButton(
-          onPressed: () {},
+          onPressed: openNoteBox,
           child: Icon(Icons.add),
         ));
   }
